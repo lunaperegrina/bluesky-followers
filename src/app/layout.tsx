47362f6d12cc3d713cd35fe2f024bbaaeb8b0410
Luna Peregrina <lunaperegrinamoon@gmail.com>
2024-12-22 14:32:27 -0300

    chore: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				{children}
+				<Analytics />
 				<Toaster />
 			</body>
 		</html>
